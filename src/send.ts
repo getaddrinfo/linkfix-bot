1be178f94afcd39b4c17514a0ec81b0f8bc93866
getaddrinfo <oss@getaddrinfo.com>
2023-12-23 19:06:20 +0000

    fix(edit): send if fail to spin for sent message info

--- src/send.ts
+++ src/send.ts
@@ -24,6 +24,13 @@ export const edit = async (
     return;
   }
 
+  // this is a bodge due to the way networking works - we
+  // may receive the `MESSAGE_UPDATE` before we have received
+  // a response for our `POST /channels/{id}/messages` (post message)
+  //
+  // To alleviate this, if we don't have it in the cache, we spin
+  // 100 times (over 10_000 milliseconds) to try and get the data
+  // needed to edit. Otherwise, we just post a new message.
   let originalMessage = await willEdit.get(forMessageId);
   if (!originalMessage) {
     for (let i = 0; i < 100; i++) {
@@ -35,11 +42,21 @@ export const edit = async (
   }
 
   if (!originalMessage) {
-    console.warn('failed to spin to get message', {
-      forMessageId,
-      guildId,
-      channelId
-    });
+    await send(
+      links,
+      { 
+        guildId, 
+        channelId, 
+        // in this case, this is fine - parentId is only used if `cleanupParentMessage` is true
+        parentId: '', 
+        // augment the features so it just sends the links that we couldn't edit into the message
+        features: {
+          cleanupParentMessage: false,
+          onlySendLinks: true,
+          waitForValidEmbed: false
+        } 
+      },
+    );
     return;
   };
 
@@ -79,7 +96,6 @@ export const send = async (
   });
 
   const id = (result as any).id;
-
 
   if (willBeEdited) {
     await willEdit.set(parentId, {
